9c2b37d9f30270b7f12f01ba88106cd953066bd8
ross <ross@b067294f-1dea-0310-9683-c47a78595994>
2007-11-07 15:38:26 +0000

    2007-11-07  Ross Burton  <ross@openedhand.com.com>
    	* gtk-im/*.[ch]:
    	Add copyright headers.
    
    
    git-svn-id: https://svn.o-hand.com/repos/matchbox/trunk/matchbox-keyboard@1800 b067294f-1dea-0310-9683-c47a78595994

--- gtk-im/im-module.c
+++ gtk-im/im-module.c
@@ -1,3 +1,22 @@
+/* 
+ *  Matchbox Keyboard - A lightweight software keyboard.
+ *
+ *  Author: Ross Burton <ross@o-hand.com>
+ *
+ *  Copyright (c) 2007 OpenedHand Ltd - http://o-hand.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ */
+
 #include <string.h>
 #include <gtk/gtkimmodule.h>
 #include "im-context.h"
@@ -6,9 +25,9 @@
 
 static const
 GtkIMContextInfo im_info = {
-  IM_CONTEXT, "Matchbox IM Invoker",
-  "", "", /* We have no user-visible strings */
-  "*"
+  IM_CONTEXT, "Virtual Keyboard",
+  /* TODO: localise the above string */
+  "", "", "*"
 };
 
 static const GtkIMContextInfo *info_list[] = { &im_info };
