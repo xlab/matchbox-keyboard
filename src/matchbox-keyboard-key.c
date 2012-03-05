9219cedaf86428e4245d60b70ea64dfbb795b7ca
Tomas Frydrych <tomas@sleepfive.com>
2012-03-05 10:32:38 +0000

    Make all modifier vkb keys sticky

--- src/matchbox-keyboard-key.c
+++ src/matchbox-keyboard-key.c
@@ -576,14 +576,17 @@ mb_kbd_key_press (MBKeyboardKey *key)
 	    break;
 	  case MBKeyboardKeyModMod1:
 	    mb_kbd_toggle_state(key->kbd, MBKeyboardStateMod1);
+            mb_kbd_set_held_key(key->kbd, key);
 	    queue_full_kbd_redraw = True;
 	    break;
 	  case MBKeyboardKeyModMod2:
 	    mb_kbd_toggle_state(key->kbd, MBKeyboardStateMod2);
+            mb_kbd_set_held_key(key->kbd, key);
 	    queue_full_kbd_redraw = True;
 	    break;
 	  case MBKeyboardKeyModMod3:
 	    mb_kbd_toggle_state(key->kbd, MBKeyboardStateMod3);
+            mb_kbd_set_held_key(key->kbd, key);
 	    queue_full_kbd_redraw = True;
 	    break;
 	  case MBKeyboardKeyModCaps:
@@ -592,9 +595,11 @@ mb_kbd_key_press (MBKeyboardKey *key)
 	    break;
           case MBKeyboardKeyModControl:
 	    mb_kbd_toggle_state(key->kbd, MBKeyboardStateControl);
+            mb_kbd_set_held_key(key->kbd, key);
 	    break;
 	  case MBKeyboardKeyModAlt:
 	    mb_kbd_toggle_state(key->kbd, MBKeyboardStateAlt);
+            mb_kbd_set_held_key(key->kbd, key);
 	    break;
 	  default:
 	    DBG("unknown modifier action");
