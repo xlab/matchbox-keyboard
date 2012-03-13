87ebe2d91d51fd17e1436f7162843bc67c9afea2
Ross Burton <ross.burton@intel.com>
2012-03-13 15:48:41 +0000

    Relicense applet/applet.c to LGPL

--- applet/applet.c
+++ applet/applet.c
@@ -7,14 +7,14 @@
  *  Ross Burton <ross@openedhand.com>
  *  Stefan Schmidt <stefan@openmoko.org>
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; version 2 of the license.
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms and conditions of the GNU Lesser General Public License,
+ *  version 2.1, as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
+ *  This program is distributed in the hope it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ *  more details.
  */
 
 #include <gtk/gtk.h>
